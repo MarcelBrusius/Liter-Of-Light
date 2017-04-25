6ac652f340343a6dc2fe9e80ff84338289be55aa
Marcel Brusius <marcel.brusius@outlook.de>
2017-04-25 16:02:30 +0200

    - added some basic ideas for light rays - added calculation of diffusive reflectance -> get surface patches which are hit by light rays

--- print3DModel.m
+++ print3DModel.m
@@ -26,7 +26,7 @@ sur = vert(x,:);
 val = sur(1,3);
 
 %Generate the "roof":
-[X,Y] = meshgrid(-20:.5:20,-20:.5:20);
+[X,Y] = meshgrid(-50:2:50,-50:2:50);
 no = zeros(1,sum(x));
 for i = 1:sum(x)
     no(i) =norm(sur(i,[1,2]));
@@ -57,12 +57,12 @@ surf(G,Surface.Roof(:,:,1),Surface.Roof(:,:,2),Surface.Roof(:,:,3),...
 hold on
 
 % Plot the bottle:
-plot(Surface.Bottle,'FaceAlpha',.4,'FaceLighting','gouraud','BackFaceLighting','unlit');
+plot(Surface.Bottle,'FaceAlpha',1,'FaceLighting','gouraud','BackFaceLighting','unlit');
 % trisurf(k,vert(:,1),vert(:,2),vert(:,3));
 % axis image
 % alpha(0.3)
 
-hold off
+hold on
 
 % boundary facets (represented by triangle vertices in every row) given in
 % "k", or accessible via alphaShape's boundaryFacets function:
@@ -73,23 +73,28 @@ Surface.Normal = zeros(size(Surface.BoundaryFacets));
 for i = 1:numel(Surface.BoundaryFacets)/3
     Surface.Normal(i,:) = cross(Surface.Vertices(Surface.BoundaryFacets(i,1),:),...
         Surface.Vertices(Surface.BoundaryFacets(i,2),:));
+    Surface.Normal(i,:) = Surface.Normal(i,:)/norm(Surface.Normal(i,:));
 end
 
 % now it's possible/easy to check for surfaces which are hit by a light ray
 % via < Light, surface_normal> > 0:
 
 % new struct for light rays:
-% Light = struct;
-% Light.Direction = [-2;-1;-2];
-% Light.Origin = 16*rand(20,3)+16;
-% 
-% lambda = 5;
-% 
-% figure(2);
-% plot3(Light.Origin(:,1),Light.Origin(:,2),Light.Origin(:,3),'x');
-% hold on
-% plot3([Light.Origin(:,1), Light.Origin(:,1)-Light.Direction(1)],...
-%       [Light.Origin(:,2), Light.Origin(:,2)-Light.Direction(2)],...
-%       [Light.Origin(:,3), Light.Origin(:,3)-Light.Direction(3)])
+Light = struct;
+Light.Direction = [0 1 2];
+Light.Direction = Light.Direction/norm(Light.Direction);
+x = [0 0.25 1.5];
+Light.Origin = 20*(2*rand(180,3)-1)+80*x/norm(x);
 
-light(G, 'Style','local','Position',[-30,-30,200], 'Color', [1,0,0]);
+lambda = 40;
+
+plot3(G,Light.Origin(:,1),Light.Origin(:,2),Light.Origin(:,3),'x');
+hold on
+for i = 1:numel(Light.Origin)/3
+    plot3(G,[Light.Origin(i,1), Light.Origin(i,1)-lambda*Light.Direction(1)],...
+          [Light.Origin(i,2), Light.Origin(i,2)-lambda*Light.Direction(2)],...
+          [Light.Origin(i,3), Light.Origin(i,3)-lambda*Light.Direction(3)], '-')
+end
+% light(G, 'Style','infinite','Position',[-30,-30,90], 'Color', [1,1,1]);
+
+Light.Reflectance = diffuse(Surface.Normal(:,1),Surface.Normal(:,2),Surface.Normal(:,2),[-30,-30,50]);
