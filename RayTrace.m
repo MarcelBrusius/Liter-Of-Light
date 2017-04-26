c4398004a8baa2d75a11b95a7e60f19878e252d1
Marcel Brusius <marcel.brusius@outlook.de>
2017-04-26 18:09:02 +0200

    - Fixes to 'RayTrace' - Fixed typo in 'refractLight'

--- RayTrace.m
+++ RayTrace.m
@@ -19,20 +19,22 @@ function [ Refraction ] = RayTrace( Surface , Light , varargin)
     
     G = gca; %get current axes
 
-    Contact.RayNumber = zeros(numel(Light.Origin)/3);
-    Contact.Vertex = zeros(size(Light.Origin));
-    Contact.BoundaryFacet = zeros(size(Light.Origin));
-    Contact.Ray_t = zeros(numel(Light.Origin)/3,1);
-    Contact.Mask = false(numel(Surface.BoundaryFacets)/3,1);
-    Contact.Facet = zeros(numel(Light.Origin),1);
+    [Origin_row, Origin_col] = size(Light.Origin);
     
-    Refraction.Direction = zeros(size(Surface.BoundaryFacets));
-    Refraction.Origin = zeros(numel(Light.Origin)/3,3);
+    Contact.RayNumber = zeros(Origin_row,1);
+    Contact.Vertex = zeros(Origin_row, Origin_col);
+    Contact.BoundaryFacet = zeros(Origin_row, Origin_col);
+    Contact.Ray_t = zeros(Origin_row,1);
+    Contact.Mask = false(Origin_row,1);
+    Contact.Facet = zeros(Origin_row,1);
+    
+    Refraction.Direction = zeros(size(Light.Origin));
+    Refraction.Origin = zeros(size(Light.Origin));
 
     for raynum = 1:numel(Light.Origin)/3
         for i = 1:numel(Surface.BoundaryFacets)/3
             rs = Light.Origin(raynum,:)' - Surface.Vertices(Surface.BoundaryFacets(i,1),:)'; % right hand side of equation
-            sysmat = [-Light.Direction', ...
+            sysmat = [-Light.Direction(raynum,:)', ...
                 Surface.Vertices(Surface.BoundaryFacets(i,2),:)'-Surface.Vertices(Surface.BoundaryFacets(i,1),:)',...
                 Surface.Vertices(Surface.BoundaryFacets(i,3),:)'-Surface.Vertices(Surface.BoundaryFacets(i,1),:)']; % System matrix of equation
             sol = sysmat\rs;
@@ -53,9 +55,9 @@ function [ Refraction ] = RayTrace( Surface , Light , varargin)
             end
         end
         if Contact.Facet(raynum)>0
-            Contact.Vertex(raynum,:) = Light.Origin(raynum,:) + Contact.Ray_t(raynum)*Light.Direction(numray,:);
+            Contact.Vertex(raynum,:) = Light.Origin(raynum,:) + Contact.Ray_t(raynum)*Light.Direction(raynum,:);
             Contact.BoundaryFacets(raynum,:) = Surface.BoundaryFacets(i,:);
-            Contact.Mask(Contact.Facet(raynum)) = true;
+            Contact.Mask(raynum) = true;
             if flag == 1
                 plot3(G,Contact.Vertex(raynum,1), Contact.Vertex(raynum,2), Contact.Vertex(raynum,3), 'r*')
             end
@@ -64,14 +66,14 @@ function [ Refraction ] = RayTrace( Surface , Light , varargin)
             nAir = 1;
             nWater = 1.33;
             Refraction.Origin = Contact.Vertex;
-            Refraction.Direction(Contact.Facet(raynum),:) = refractLight(Surface.Normal(Contact.Facet(raynum),:), Light.Direction(raynum,:), nAir, nWater);
+            Refraction.Direction(raynum,:) = refractLight(Surface.Normal(Contact.Facet(raynum),:), Light.Direction(raynum,:), nAir, nWater);
     %         Reflection.Direction = reflectLight(Contact, Light, nAir, nWater);
             plot3(G,Light.Origin(raynum,1),Light.Origin(raynum,2),Light.Origin(raynum,3),'bx');
             hold on
 
-            plot3(G,[Light.Origin(raynum,1), Light.Origin(raynum,1)+Contact.Ray_t(raynum)*Light.Direction(numray,1)],...
-                    [Light.Origin(raynum,2), Light.Origin(raynum,2)+Contact.Ray_t(raynum)*Light.Direction(numray,2)],...
-                    [Light.Origin(raynum,3), Light.Origin(raynum,3)+Contact.Ray_t(raynum)*Light.Direction(numray,3)], 'b-')
+            plot3(G,[Light.Origin(raynum,1), Light.Origin(raynum,1)+Contact.Ray_t(raynum)*Light.Direction(raynum,1)],...
+                    [Light.Origin(raynum,2), Light.Origin(raynum,2)+Contact.Ray_t(raynum)*Light.Direction(raynum,2)],...
+                    [Light.Origin(raynum,3), Light.Origin(raynum,3)+Contact.Ray_t(raynum)*Light.Direction(raynum,3)], 'b-')
         end
     end
     Refraction.Direction = Refraction.Direction(Contact.Mask,:);
