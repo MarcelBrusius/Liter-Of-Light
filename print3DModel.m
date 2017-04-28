fd2eb379c1ae183630f2e5590f593b937cce4ae9
Marcel Brusius <marcel.brusius@outlook.de>
2017-04-28 15:59:45 +0200

    - Updated script for calling 'RayTrace' - Added binary tree-like structure for computing both reflected and refracted light rays - Transmitting light rays not working yet -> reflectLight has to be updated

--- print3DModel.m
+++ print3DModel.m
@@ -17,7 +17,7 @@ Surface = createSurface(vert);
 
 % new struct for light rays:
 x = [0 0.25 1.5];
-Origin = 5*(2*rand(1000,3)-1)+80*x/norm(x);
+Origin = 5*(2*rand(300,3)-1)+80*x/norm(x);
 % Origin = [0.5 0 5] + 80*x/norm(x,2);
 % Origin = [0.5 13.15 83.91];
 Direction = [0 -.5 -2];
@@ -26,13 +26,25 @@ Light = createLight(Direction,Origin);
 %calculate facets seen by light:
 % Surface.Illuminated = Surface.BoundaryFacets(Surface.Normal*Light.Direction' < 0);
 
-FirstRefract = RayTrace(Surface,Light,1,'b');
+[Refract, Reflect] = RayTrace(Surface,Light,1);
 
-SecondRefract = RayTrace(Surface,FirstRefract,1,'r');
+height = 2;
 
-% ThridRefract = RayTrace(Surface,SecondRefract,1,'y');
+Ind = [2*(1:(2^(height-1)-1))', 2*(1:(2^(height-1)-1))'+1];
+Ind(Ind>(2^(height)-1)) = 0;
 
+C = cell(2^(height)-1,1);
+C{1} = [Refract,Reflect];
 
-% 
+for i = 2:2:2^(height)-1
+    [T,R] = RayTrace(Surface,C{floor(i/2)}(1),1);
+    C{ i } = [T,R];
+    
+    [T,R] = RayTrace(Surface,C{floor(i/2)}(2),1);
+    C{i+1} = [T,R];
+end
 
-
+for i = 2^(height-1):2^(height)-1
+    RayTrace(Surface,C{i}(1),1);
+    RayTrace(Surface,C{i}(2),1);
+end
