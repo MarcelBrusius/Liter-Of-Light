65499aad0aeed4d9367a806c2d6161cd22da107d
Marcel Brusius <marcel.brusius@outlook.de>
2017-04-28 18:04:21 +0200

    - Updated function description - Set refraction direction to [0,0,0] if total reflection is observed

--- snellsLaw.m
+++ snellsLaw.m
@@ -1,11 +1,11 @@
 function [ Reflection, Refraction ] = snellsLaw( Normal, Direction, n1, n2 )
-%REFLECTLIGHT Calculate reflection of light at a surface
-%   Compute reflection of light at a surface of an object given by
-%   triangular patches.
+%REFLECTLIGHT Calculate reflection and refraction of light at a surface
+%   Compute reflection and refraction of light at a surface of an object 
+%   given by triangular patches.
 %
 %   Light direction is given by Light.Direction, surface normal is given by
-%   Surface.Normal. (The refraction coefficients are given by nOutside and
-%   nInside for the respective media).
+%   Surface.Normal. (The refraction coefficients are given by n1 and n2 for
+%   the respective media).
 
 % Reflection = Direction - 2*dot(Normal,Direction)/(norm(Normal,2)^2)*Normal;
 
@@ -15,6 +15,8 @@ cos_theta_2 = sqrt(1-(n1/n2)^2*(1-cos_theta_1^2));
 if cos_theta_1 > 0
     Refraction = (n1/n2)*Direction + ((n1/n2)*cos_theta_1 - cos_theta_2)*Normal;
     Refraction = Refraction/norm(Refraction,2);
+elseif cos_theta_1 > 1
+    Refraction = [0, 0, 0];
 else
     Refraction = (n1/n2)*Direction + ((n1/n2)*cos_theta_1 + cos_theta_2)*Normal;
     Refraction = Refraction/norm(Refraction,2);
@@ -22,7 +24,5 @@ end
 
 Reflection = Direction + 2*cos_theta_1*Normal;
 Reflection = Reflection/norm(Reflection,2);
-
-
 end
 
