6786bd6b104794497c96a24b9075982fe435558a
Marcel Brusius <marcel.brusius@outlook.de>
2017-04-29 13:05:29 +0200

    - Added color choice for printing rays (easier debugging)

--- printRays.m
+++ printRays.m
@@ -1,4 +1,4 @@
-function printRays( Light, lambda )
+function printRays( Light, lambda, color )
 %UNTITLED2 Summary of this function goes here
 %   Detailed explanation goes here
 
@@ -6,8 +6,8 @@ G = gca;
 
 [n,~] = size(Light.Origin);
 for raynum = 1:n
-    plot3(G,[Light.Origin(raynum,1), Light.Origin(raynum,1)+lambda*Light.Direction(raynum,1)],...
-            [Light.Origin(raynum,2), Light.Origin(raynum,2)+lambda*Light.Direction(raynum,2)],...
-            [Light.Origin(raynum,3), Light.Origin(raynum,3)+lambda*Light.Direction(raynum,3)], 'b-')
+    plot3(G,[Light.Origin(raynum,1), Light.Origin(raynum,1)+lambda(raynum)*Light.Direction(raynum,1)],...
+            [Light.Origin(raynum,2), Light.Origin(raynum,2)+lambda(raynum)*Light.Direction(raynum,2)],...
+            [Light.Origin(raynum,3), Light.Origin(raynum,3)+lambda(raynum)*Light.Direction(raynum,3)], color)
 end
 
