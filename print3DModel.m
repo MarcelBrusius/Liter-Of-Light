77e91f90b99ef8d7c8a23135444f12de4c6f5630
Marcel Brusius <marcel.brusius@outlook.de>
2017-04-29 22:46:25 +0200

    - Added more realistic bottle model - Added waitbar in order to show progress of the computation

--- print3DModel.m
+++ print3DModel.m
@@ -1,11 +1,11 @@
 clear
 
-% Obj = stlread('Cocacola_bottle.stl');
+% Obj = stlread('Bottle_Cola.stl');
 % vert = unique(Obj.vertices,'rows');
 % csvwrite('ExportBottle.dat',vert(:,3))
 
-% save('Bottle.mat',vert)
-loaded = load('Bottle.mat');
+% save('Bottle2.mat',vert)
+loaded = load('Bottle2.mat');
 vert = loaded.vert;
 
 F = figure(1);
@@ -19,7 +19,7 @@ Surface = createSurface(vert);
 
 % new struct for light rays:
 x = [0 0.5 1.25];
-Origin = 5*(2*rand(1000,3)-1)+80*x/norm(x);
+Origin = 5*(2*rand(10,3)-1)+160*x/norm(x);
 % Origin = 5*(2*x/norm(x)-1)+80*x/norm(x);
 Direction = [0 -.75 -1];
 Light = createLight(Direction,Origin);
@@ -38,6 +38,7 @@ height = 10;
 % C = cell(2^(height)-1,2);
 
 above_roof = 40;
+h = waitbar(0,['Calculating...', num2str(0), '%']);
 
 for i = 2:height
     [T,R] = RayTrace(Surface,R);
@@ -48,9 +49,11 @@ for i = 2:height
     T.Origin = T.Origin(T.Origin(:,3)<above_roof,:);
     printRays(T,20,'b-');
 %     C{i,2} = R;
+    waitbar(i/height,h,['Calculating...', num2str(100*i/height), '%']);
 end
+close(h)
 
-G.ZLim = [-20, 80];
-G.XLim = [-25, 25];
-G.YLim = [-25, 25];
+% G.ZLim = [-20, 80];
+% G.XLim = [-25, 25];
+% G.YLim = [-25, 25];
 
