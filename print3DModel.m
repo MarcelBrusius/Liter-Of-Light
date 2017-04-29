53232df9a181aa3a5c21f7a796baaf0c8bc02ef3
Marcel Brusius <marcel.brusius@outlook.de>
2017-04-30 00:13:14 +0200

    - Use original bottle model again - Change roof height to 45 - Added time measurement - Only use initial hitting points on surface above roof threshold

--- print3DModel.m
+++ print3DModel.m
@@ -1,11 +1,11 @@
 clear
-
+tic
 % Obj = stlread('Bottle_Cola.stl');
 % vert = unique(Obj.vertices,'rows');
 % csvwrite('ExportBottle.dat',vert(:,3))
 
 % save('Bottle2.mat',vert)
-loaded = load('Bottle2.mat');
+loaded = load('Bottle.mat');
 vert = loaded.vert;
 
 F = figure(1);
@@ -17,18 +17,26 @@ init = struct;
 
 Surface = createSurface(vert);
 
+clear('vert');
+
 % new struct for light rays:
 x = [0 0.5 1.25];
-Origin = 5*(2*rand(10,3)-1)+160*x/norm(x);
-% Origin = 5*(2*x/norm(x)-1)+80*x/norm(x);
+% Origin = 20*(2*rand(10,3)-1)+100*x/norm(x);
+Origin = 5*(2*x/norm(x)-1)+100*x/norm(x);
 Direction = [0 -.75 -1];
 Light = createLight(Direction,Origin);
 
 [R,T] = RayTrace(Surface,Light);
+above_roof = 45;
+R.Direction = R.Direction(R.Origin(:,3)>above_roof,:);
+T.Direction = T.Direction(T.Origin(:,3)>above_roof,:);
+
+R.Origin = R.Origin(R.Origin(:,3)>above_roof,:);
+T.Origin = T.Origin(T.Origin(:,3)>above_roof,:);
 
 init.Origin = R.Origin;
-init.Direction = Light.Direction;
-printRays(init,-30,'y-');
+init.Direction = Light.Direction(R.Origin(:,3)>above_roof,:);
+printRays(init,-20,'y-');
 
 height = 10;
 
@@ -37,7 +45,7 @@ height = 10;
 
 % C = cell(2^(height)-1,2);
 
-above_roof = 40;
+
 h = waitbar(0,['Calculating...', num2str(0), '%']);
 
 for i = 2:height
@@ -53,7 +61,10 @@ for i = 2:height
 end
 close(h)
 
-% G.ZLim = [-20, 80];
-% G.XLim = [-25, 25];
-% G.YLim = [-25, 25];
+view(3)
 
+G.ZLim = [-20, 80];
+G.XLim = [-25, 25];
+G.YLim = [-25, 25];
+
+view(3);
