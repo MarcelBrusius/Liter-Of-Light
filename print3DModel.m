2493b448b97f25962ec220aa99c56da8069101a3
Jannik Altevogt <jannik.altevogt@gmail.com>
2017-05-16 23:32:10 +0200

    Rays with too low intensity will now be ignored; added for-loops for different incidence angles and different heights of the roof.

--- print3DModel.m
+++ print3DModel.m
@@ -8,11 +8,20 @@ tic
 loaded = load('Bottle.mat');
 vert = 0.5*loaded.vert;
 
-above_roof = 10;
-%elevation angle,i.e. incidence angle w.r.t. the horizontal
-IncidenceAngle=65;
+
+% Initialize some data vectors for later analysis
+above_roof_vec=0:7:28; % choose numbers in [0,29]
+IncidenceAngleVec=180/pi*acos([0.197,0.424,0.627,0.792,0.908,0.968,0.966,0.904,0.785,0.618,0.413,0.186]); %Values for Venezuela (Caracas), June 16th 2017, [7,...,18] o'clock
+InBottleVec=zeros(length(above_roof_vec),length(IncidenceAngleVec));
+OnBottleVec=zeros(length(above_roof_vec),length(IncidenceAngleVec));
+BottleIntensityVec=zeros(length(above_roof_vec),length(IncidenceAngleVec));
+for i = 1:length(above_roof_vec)
+above_roof=above_roof_vec(i);
+for j=1:length(IncidenceAngleVec) %elevation angle,i.e. incidence angle w.r.t. the horizontal
+IncidenceAngle=IncidenceAngleVec(j);
+    
 %number of sun rays
-n_rays=500;
+n_rays=20;
 
 [Origin,Direction,Intensity] = chooseRays(above_roof, IncidenceAngle, n_rays);
 Direction=Direction/norm(Direction);
@@ -26,13 +35,10 @@ init = struct;
 
 Surface = createSurface(vert);
 
-clear('vert');
-
 Light = createLight(Direction,Origin);
 Light.Intensity=repmat(Intensity,[n_rays,1]);
 
-[R,T] = RayTrace(Surface,Light);
-disp(length(R.Origin))
+[R,T] = RayTrace(Surface,Light);  % R represents the rays that are transmitted, T the light rays that are reflected, (but the implementation is correct i think)
 
 % ignore rays that are above the "imaginary roof"
 R.Direction = R.Direction(R.Origin(:,3)>above_roof,:);
@@ -54,6 +60,10 @@ T.Intensity=T.Intensity(T.Origin(:,3)<28.1,:);
 
 R.Origin = R.Origin(R.Origin(:,3)<28.1,:);
 T.Origin = T.Origin(T.Origin(:,3)<28.1,:);
+OnBottleVec(i,j)=sum(R.Intensity)+sum(T.Intensity);
+disp(['Radient flux on bottle surface: ',num2str(OnBottleVec(i,j))])
+InBottleVec(i,j)=sum(R.Intensity);
+disp(['Radient flux transmitted into the bottle: ',num2str(InBottleVec(i,j))])
 
 init.Origin = R.Origin;
 init.Direction=Light.Direction;
@@ -74,22 +84,34 @@ height = 10; % number of iterations of refraction/reflection
 h = waitbar(0,['Calculating...', num2str(0), '%']);
 
 BottleIntensity=0;
-for i = 2:height
+for k = 2:height
         [T,R] = RayTrace(Surface,R);
 %     C{floor(i/2)
 %     C{i,1} = T;
+    % reflected rays, that carry too little power, will be ignored
+    threshold=0.01/n_rays; % We will neglect <1% of the power in the bottle
+    R.Direction = R.Direction(R.Intensity>threshold,:);
+    R.Origin = R.Origin(R.Intensity>threshold,:);
+    R.Intensity = R.Intensity(R.Intensity>threshold,:);
+    
     % print rays, and ignore those originating under the "roof" (threshold):
     T.Direction = T.Direction(T.Origin(:,3)<above_roof,:);
-    T.Origin = T.Origin(T.Origin(:,3)<above_roof,:);
     T.Intensity = T.Intensity(T.Origin(:,3)<above_roof,:);
+    T.Origin = T.Origin(T.Origin(:,3)<above_roof,:);
     %disp(length(T.Origin))
     %printRays(T,10,'b-');
 %     C{i,2} = R;
-    waitbar(i/height,h,['Calculating...', num2str(100*i/height), '%']);
+    waitbar(k/height,h,['Calculating...', num2str(100*k/height), '%']);
     BottleIntensity=BottleIntensity + sum(T.Intensity);
-    disp(sum(T.Intensity));
+    disp(['Iteration: ',num2str(k),', Radient flux emitted from bottle under the roof: ',num2str(sum(T.Intensity))]);
 end
+BottleIntensityVec(i,j)=BottleIntensity;
+disp(['Total radient flux emitted from bottle under the roof: ',num2str(BottleIntensity)])
 close(h)
+end
+end
+
+clear('vert');
 
 % forces 3D view
 view(3)
