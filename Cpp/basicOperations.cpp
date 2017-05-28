e867f36ea38bc525d712a0fb3d7aee375e1273f3
Marcel Brusius <marcel.brusius@outlook.de>
2017-05-28 21:55:33 +0200

    - Functions overloaded

---
+++ Cpp/basicOperations.cpp
@@ -0,0 +1,279 @@
+#include <iostream>
+#include <cmath>
+
+using namespace std; // vermeidet Doppelbenennung von Variablen
+
+
+#include "basicOperations.h"
+
+void printVector( vector<double>& a ) {
+	cout << "[";
+	for (int i=0; i<a.size(); i++) {
+		if (i+1<a.size()) {
+		    cout << a[i] << ",";
+		}else{
+			cout << a[i];
+		}
+	}
+	cout << "]";
+}
+
+void printVector(vector<int>& a) {
+	cout << "[";
+	for (int i = 0; i<a.size(); i++) {
+		if (i + 1<a.size()) {
+			cout << a[i] << ",";
+		}
+		else {
+			cout << a[i];
+		}
+	}
+	cout << "]";
+}
+
+// normiere Vektor in euklidischer Norm
+vector<double> normVector( vector<double>& a ) 
+{
+	if( a.size()==0 ) {
+		cout << "Error: 'normVector' mit leerem Vektor aufgerufen.\n";
+	}
+	double norm=0;
+	for(int i=0; i<a.size(); i++ ) {
+		norm += pow(a[i],2);
+	}
+	norm=sqrt(norm);
+	for(int i=0; i<a.size(); i++ ) {
+		a[i]=a[i]/norm;
+	}
+	return a;
+}
+
+vector<double> normVector(vector<int>& a)
+{
+	vector<double> b(a.size());
+	if (a.size() == 0) {
+		cout << "Error: 'normVector' mit leerem Vektor aufgerufen.\n";
+	}
+	double norm = 0;
+	for (int i = 0; i<a.size(); i++) {
+		norm += pow((double)a[i], 2);
+	}
+	norm = sqrt(norm);
+	for (int i = 0; i<a.size(); i++) {
+		b[i] = (double)a[i] / norm;
+	}
+	return b;
+}
+
+double sumVector( vector<double>& a ) 
+{
+	if( a.size()==0 ) {
+		cerr << "Error: Empty vector.\n";
+	}
+	double s=0;
+	for(int i=0; i<a.size(); i++ ) {
+		s+=a[i];
+	}
+	return s;
+}
+
+double sumVector(vector<int>& a) 
+{
+	if (a.size() == 0) {
+		cerr << "Error: Empty vector.\n";
+	}
+	double s = 0;
+	for (int i = 0; i<a.size(); i++) {
+		s += (double)a[i];
+	}
+	return s;
+}
+
+vector<double> addVectors( vector<double>& a , vector<double>& b ) 
+{
+	if( a.size()==0 || b.size()==0 ) {
+		cerr << "Error: Empty vector.\n";
+	}
+	if (a.size()!=b.size()) {
+		cerr << "Error: Vectors of different size.\n";
+	}
+	vector<double> s(a.size(),0);
+	for (int i=0; i<a.size(); i++) {
+		s[i]=a[i]+b[i];
+	}
+	return s;
+}
+
+vector<double> addVectors(vector<int>& a, vector<int>& b) 
+{
+	if (a.size() == 0 || b.size() == 0) {
+		cerr << "Error: Empty vector.\n";
+	}
+	if (a.size() != b.size()) {
+		cerr << "Error: Vectors of different size.\n";
+	}
+	vector<double> s(a.size(), 0);
+	for (int i = 0; i<a.size(); i++) {
+		s[i] = (double)a[i] + (double)b[i];
+	}
+	return s;
+}
+
+vector<double> addVectors(vector<double>& a, vector<int>& b)
+{
+	if (a.size() == 0 || b.size() == 0) {
+		cerr << "Error: Empty vector.\n";
+	}
+	if (a.size() != b.size()) {
+		cerr << "Error: Vectors of different size.\n";
+	}
+	vector<double> s(a.size(), 0);
+	for (int i = 0; i<a.size(); i++) {
+		s[i] = a[i] + (double)b[i];
+	}
+	return s;
+}
+
+vector<double> addVectors(vector<int>& a, vector<double>& b)
+{
+	if (a.size() == 0 || b.size() == 0) {
+		cerr << "Error: Empty vector.\n";
+	}
+	if (a.size() != b.size()) {
+		cerr << "Error: Vectors of different size.\n";
+	}
+	vector<double> s(a.size(), 0);
+	for (int i = 0; i<a.size(); i++) {
+		s[i] = (double)a[i] + b[i];
+	}
+	return s;
+}
+
+vector<double> addVectorScalar( vector<double>& a, double& b ) 
+{
+	if( a.size()==0 ) {
+		cerr << "Error: Empty vector.\n";
+	}
+	vector<double> s(a.size(),0);
+	for (int i=0; i<a.size(); i++) {
+		s[i]=a[i]+b;
+	}
+	return s;
+}
+
+vector<double> addVectorScalar(vector<int>& a, double& b)
+{
+	if (a.size() == 0) {
+		cerr << "Error: Empty vector.\n";
+	}
+	vector<double> s(a.size(), 0);
+	for (int i = 0; i<a.size(); i++) {
+		s[i] = (double)a[i] + b;
+	}
+	return s;
+}
+
+vector<double> addVectorScalar(vector<double>& a, int& b)
+{
+	if (a.size() == 0) {
+		cerr << "Error: Empty vector.\n";
+	}
+	vector<double> s(a.size(), 0);
+	for (int i = 0; i<a.size(); i++) {
+		s[i] = a[i] + (double)b;
+	}
+	return s;
+}
+
+vector<double> addVectorScalar(vector<int>& a, int& b)
+{
+	if (a.size() == 0) {
+		cerr << "Error: Empty vector.\n";
+	}
+	vector<double> s(a.size(), 0);
+	for (int i = 0; i<a.size(); i++) {
+		s[i] = a[i] + b;
+	}
+	return s;
+}
+
+// Elementweises multiplizieren
+vector<double> multVectors( vector<double>& a, vector<double>& b ) 
+{
+	if( a.size()==0 || b.size()==0 ) {
+		cerr << "Error: Empty vector.\n";
+	}
+	if (a.size()!=b.size()) {
+		cerr << "Error: Vectors of different size.\n";
+	}
+	vector<double> m(a.size(),0);
+	for (int i=0; i<a.size(); i++) {
+		m[i]=a[i]*b[i];
+	}
+	return m;
+}
+
+vector<double> multVectors(vector<int>& a, vector<double>& b) 
+{
+	if (a.size() == 0 || b.size() == 0) {
+		cerr << "Error: Empty vector.\n";
+	}
+	if (a.size() != b.size()) {
+		cerr << "Error: Vectors of different size.\n";
+	}
+	vector<double> m(a.size(), 0);
+	for (int i = 0; i<a.size(); i++) {
+		m[i] = (double)a[i] * b[i];
+	}
+	return m;
+}
+
+vector<double> multVectors(vector<double>& a, vector<int>& b) 
+{
+	if (a.size() == 0 || b.size() == 0) {
+		cerr << "Error: Empty vector.\n";
+	}
+	if (a.size() != b.size()) {
+		cerr << "Error: Vectors of different size.\n";
+	}
+	vector<double> m(a.size(), 0);
+	for (int i = 0; i<a.size(); i++) {
+		m[i] = a[i] * (double)b[i];
+	}
+	return m;
+}
+
+vector<double> multVectorScalar( vector<double>& a, double& b ) {
+	if( a.size()==0 ) {
+		cerr << "Error: Empty vector.\n";
+	}
+	vector<double> m(a.size(),0);
+	for (int i=0; i<a.size(); i++) {
+		m[i]=a[i]*b;
+	}
+	return m;
+}
+
+vector<double> multVectorScalar(vector<int>& a, double& b) {
+	if (a.size() == 0) {
+		cerr << "Error: Empty vector.\n";
+	}
+	vector<double> m(a.size(), 0);
+	for (int i = 0; i<a.size(); i++) {
+		m[i] = (double)a[i] * b;
+	}
+	return m;
+}
+
+vector<double> multVectorScalar(vector<double>& a, int& b) {
+	if (a.size() == 0) {
+		cerr << "Error: Empty vector.\n";
+	}
+	vector<double> m(a.size(), 0);
+	for (int i = 0; i<a.size(); i++) {
+		m[i] = a[i] * (double)b;
+	}
+	return m;
+}
+
+
--- Cpp/basicOperations.h
+++ Cpp/basicOperations.h
@@ -1,29 +1,55 @@
 /*
  * basicOperations.h
  *
- *  Created on: 21.05.2017
- *      Author: Jannik
+ *  Updated on: 28.05.2017
+ *      Author: Jannik, Marcel
  */
 
 #ifndef BASICOPERATIONS_H_
 #define BASICOPERATIONS_H_
 
-
 #include <vector> // ermöglicht die Initialisierung von Vektoren zusammen mit einigen Vektorenoperationen z.B. size
 
-void printVector( vector<double>& a );
+void printVector(vector<double>& a);
 
-vector<double> normVector( vector<double>& a );
+void printVector(vector<int>& a);
 
-double sumVector( vector<double>& a );
+// normiere Vektor in euklidischer Norm
+vector<double> normVector(vector<double>& a);
 
-vector<double> addVectors( vector<double>& a , vector<double>& b );
+vector<double> normVector(vector<int>& a);
 
-vector<double> addVectorScalar( vector<double>& a, double& b );
+double sumVector(vector<double>& a);
 
-vector<double> multVectors( vector<double>& a, vector<double>& b );
+double sumVector(vector<int>& a);
 
-vector<double> multVectorScalar( vector<double>& a, double& b );
+vector<double> addVectors(vector<double>& a, vector<double>& b);
 
+vector<double> addVectors(vector<int>& a, vector<int>& b);
+
+vector<double> addVectors(vector<double>& a, vector<int>& b);
+
+vector<double> addVectors(vector<int>& a, vector<double>& b);
+
+vector<double> addVectorScalar(vector<double>& a, double& b);
+
+vector<double> addVectorScalar(vector<int>& a, double& b);
+
+vector<double> addVectorScalar(vector<double>& a, int& b);
+
+vector<double> addVectorScalar(vector<int>& a, int& b);
+
+// Elementweises multiplizieren
+vector<double> multVectors(vector<double>& a, vector<double>& b);
+
+vector<double> multVectors(vector<int>& a, vector<double>& b);
+
+vector<double> multVectors(vector<double>& a, vector<int>& b);
+
+vector<double> multVectorScalar(vector<double>& a, double& b);
+
+vector<double> multVectorScalar(vector<int>& a, double& b);
+
+vector<double> multVectorScalar(vector<double>& a, int& b);
 
 #endif /* BASICOPERATIONS_H_ */
